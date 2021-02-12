a294c8bb29cf32f613aca0ba510783b743acc304
Maurizio Zucchelli <mauzuc90@yahoo.it>
2021-02-12 18:12:30 +0000

    feat: add a simple main to fetch and show an image

--- inspiro-frame/src/main.rs
+++ inspiro-frame/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use simplelog::{Config, LevelFilter, SimpleLogger};
+use waveshare_epd::epd_2in7b as epd;
+
+#[tokio::main]
+pub async fn main() -> anyhow::Result<()> {
+    SimpleLogger::init(LevelFilter::Info, Config::default())?;
+
+    let image = inspiro_bot::generate_image().await?.into_rgb8();
+    let image = image::imageops::thumbnail(&image, epd::EPD_HEIGHT as u32, epd::EPD_WIDTH as u32);
+
+    let (black, red) = rgb2bwr::to_bwr_split(image, true);
+    let black = epd::pack_buffer(&black).unwrap();
+    let red = epd::pack_buffer(&red).unwrap();
+
+    let mut epd = epd::Epd::new()?;
+    epd.init()?;
+    epd.display(black.iter().copied(), red.iter().copied())?;
+    epd.sleep()?;
+
+    Ok(())
 }
