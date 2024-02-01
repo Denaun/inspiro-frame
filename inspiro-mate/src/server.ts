1dce8418155927f05b80a76188020ea4e9adc3b9
Maurizio Zucchelli <mauzuc90@yahoo.it>
2024-02-01 20:55:21 +0000

    Add an endpoint to take screenshots.

--- inspiro-mate/src/server.ts
+++ inspiro-mate/src/server.ts
@@ -1,8 +1,10 @@
 import { Application, Router } from "https://deno.land/x/oak@v13.0.0/mod.ts";
+import { screenshotMiddleware } from "./chrome.ts";
 import { inspiroBotMiddleware } from "./inspiro_bot.ts";
 
 const router = new Router();
 router.get("/inspiration", inspiroBotMiddleware());
+router.get("/screenshot", await screenshotMiddleware());
 
 const app = new Application();
 app.use(router.routes());
