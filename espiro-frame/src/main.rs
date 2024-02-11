c190123a08d9cae40442214595a6da2ad9011f3a
Maurizio Zucchelli <mauzuc90@yahoo.it>
2024-02-11 09:48:27 +0000

    Clear the display.

--- espiro-frame/src/main.rs
+++ espiro-frame/src/main.rs
@@ -1,10 +1,25 @@
-fn main() {
+use esp_idf_svc::{
+    hal::{peripherals::Peripherals, task::block_on},
+    log::EspLogger,
+};
+use waveshare_epd::epd_12in48b::Epd;
+
+fn main() -> anyhow::Result<()> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
 
     // Bind the log crate to the ESP Logging facilities
-    esp_idf_svc::log::EspLogger::initialize_default();
+    EspLogger::initialize_default();
+    EspLogger {}.set_target_level("waveshare_epd", log::LevelFilter::Debug)?;
 
-    log::info!("Hello, world!");
+    let peripherals = Peripherals::take()?;
+    let mut epd = Epd::new(peripherals.spi3, peripherals.pins)?;
+    block_on(async move {
+        epd.init()?;
+        epd.clear()?;
+        epd.turn_on().await?;
+        epd.sleep()?;
+        Ok(())
+    })
 }
